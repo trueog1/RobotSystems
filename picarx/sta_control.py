de501d0823c08cdea2625ca75161a54852ad1756
trueog1 <gehrkeo@oregonstate.edu>
2025-01-28 11:10:01 -0800

    Sense-think-act update

--- picarx/sta_control.py
+++ picarx/sta_control.py
@@ -1,10 +1,10 @@
 from picarx_improved import Picarx
 import time
-import logging
+#import logging
 
-logging_format = "%(asctime)s: %(message)s"
+'''logging_format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logging_format, level = logging.INFO, datefmt="%H:%M:%S")
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)'''
 
 class Sense(object):
     def __init__(self):
@@ -15,13 +15,6 @@ class Sense(object):
 
 class Interp(object):
     def __init__(self, sensitivity = [0, 3600], polarity = False):
-        ''' Initialize Interpreter
-        
-        param sensitivity: Indicates range of acceptable light -> dark values
-        type sensitivity: list[int, int]
-        param polarity: False indicates light floor, dark line, True indicates dark floor, light line
-        type polarity: Bool      
-        '''
         self.polarity = polarity
         self.low_sense, self.high_sense = sensitivity
         self.robot_position = 0
@@ -33,22 +26,46 @@ class Interp(object):
             gs = [abs(gs - max(gs)) for gs in gs]
 
         left, middle, right = gs
-        tolerance = 50
-        center_left = left - middle
-        center_right = right - middle
-        total = left - right
 
-        if right > left:
-            self.robot_position = (right - middle)/max(right, middle)
+        if right >= left:
+            self.robot_position = (middle - right)/max(right, middle)
+            
+            if self.robot_position < 0:
+                self.robot_position = -1 * self.robot_position
+                return
+            else:
+                self.robot_position = 1 - self.robot_position
+                return
+        
+        elif left > right:
+            self.robot_position = (middle - left)/max(left, middle)
 
-    
-    def robot_position(self, kp = 10, ):
+            if self.robot_position < 0:
+                self.robot_position = self.robot_position
+
+            else:
+                self.robot_position = self.robot_position - 1
+                return
+
+    def robot_position(self ):
         return self.robot_position
                
 
 class Control(object):
-    def __init__(self):
+    def __init__(self, threshold, kp, ki):
         self.px = Picarx()
+        self.threshold = threshold
+        self.kp = kp
+        self.ki = ki
+        self.e = 0.0
+        self.angle = 0.0
+
+    def auto_steering(self, position, px):
+        if abs(position) > self.threshold:
+            self.e = self.e + position
+            self.angle = (self.kp * position) + (self.ki * self.e)
+            px.set_dir_servo_angle(self.angle)
+            return self.angle
 
 if __name__ == "__main__":
     px = Picarx()
